479ac84161817fb82af10940119a334364963919
amorim.pedro@engenharia.ufjf.br <amorim.pedro@engenharia.ufjf.br>
2022-05-22 22:55:05 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/upload_files", upload.array("files"), uploadFiles);
+
+function uploadFiles(req, res) {
+    console.log(req.body);
+    console.log(req.files);
+    res.json({ message: "Successfully uploaded files" });
+}
+app.listen(3000, () => {
+    console.log(`Server started...`);
+});
